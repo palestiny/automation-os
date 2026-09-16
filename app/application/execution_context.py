7745e8eba32c1b721b373b9e9abd121234abf599
palestiny <153906782+palestiny@users.noreply.github.com>
2026-09-16 11:19:37 -0700

    refactor: remove obsolete application Execution Context

--- app/application/execution_context.py
+++
@@ -1,9 +0,0 @@
-class ExecutionContext:
-    def __init__(self) -> None:
-        self._data = {}
-
-    def set(self, key: str, value) -> None:
-        self._data[key] = value
-
-    def get(self, key: str):
-        return self._data[key]
